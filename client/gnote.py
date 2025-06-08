d772ee69c3f7d03e3c54de00d4a6297523dc3a42
Marat Gabitov <gabitovmt@yandex.ru>
2025-06-08 23:08:26 +0500

    Реализовал более "умный" интерфейс

--- client/gnote.py
+++ client/gnote.py
@@ -18,6 +18,7 @@ class MainWindow(Tk):
         super().__init__()
 
         self.__key_var = StringVar()
+        self.__is_modified = False
 
         self.geometry("640x480")
 
@@ -26,7 +27,7 @@ class MainWindow(Tk):
 
         self.__open_btn = Button(self.__top_panel, text='open', command=self.__on_open_btn)
         self.__open_btn.pack(side='left')
-        self.__save_btn = Button(self.__top_panel, text='save', command=self.__on_save_btn)
+        self.__save_btn = Button(self.__top_panel, text='save', command=self.__on_save_btn, state='disabled')
         self.__save_btn.pack(side='left')
 
         self.__key_entry = Entry(self.__top_panel, textvariable=self.__key_var)
@@ -44,17 +45,31 @@ class MainWindow(Tk):
             wrap='word',
             spacing1=4,
             spacing2=2,
-            spacing3=4
+            spacing3=4,
+            state='disabled'
         )
         self.__editor.pack(fill='both', expand=True)
+        self.__editor.bind('<<Modified>>', lambda e: self.__on_modified())
 
         self.mainloop()
 
     def __on_open_btn(self):
+        self.__editor.config(state='normal')
         self.__editor_text = open_text()
+        self.__on_not_modified()
 
     def __on_save_btn(self):
         save_text(self.__editor_text)
+
+    def __on_not_modified(self):
+        self.__is_modified = False
+        self.__save_btn.config(state='disabled')
+
+    def __on_modified(self):
+        if self.__is_modified:
+            return
+        self.__is_modified = True
+        self.__save_btn.config(state='normal')
 
     @property
     def __editor_text(self):
