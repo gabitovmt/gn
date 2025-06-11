f3330303bbabf91d8525a16a2e43c698f6a859c0
Marat Gabitov <gabitovmt@yandex.ru>
2025-06-11 16:46:37 +0500

    Реализовал приложение сохранения заметок

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
+import json
 import sqlite3
 from typing import Annotated, Any
+
 from cryptography.fernet import Fernet
 from fastapi import FastAPI, Response, HTTPException, File
 from fastapi.responses import FileResponse
-import json
 
 # Запуск сервера uvicorn main:app --reload
 app = FastAPI()
@@ -35,14 +36,37 @@ def run(file: Annotated[bytes, File()], response: Response):
         raise HTTPException(status_code=400, detail='no request body')
 
     try:
-        data = crypto_service.decrypt(file)
+        req = crypto_service.decrypt(file)
     except BaseException:
-        raise HTTPException(status_code=400, detail='no valid data')
+        raise HTTPException(status_code=400, detail='invalid request')
 
-    print(data)
+    if 'object' not in req and 'action' not in req:
+        raise HTTPException(status_code=400, detail='invalid request')
 
-    response.status_code = 202
-    return crypto_service.encrypt(data)
+    resp = None
+    if req['object'] == 'note':
+        if req['action'] == 'get_all':
+            resp = note_service.get_all()
+        elif req['action'] == 'get_one':
+            resp = note_service.get_one(req['args']['id'])
+        elif req['action'] == 'post':
+            resp = note_service.post(req['args'])
+            response.status_code = 201
+        elif req['action'] == 'put':
+            note_service.put(req['args'])
+            response.status_code = 202
+        elif req['action'] == 'delete':
+            note_service.delete(req['args']['id'])
+            response.status_code = 204
+        else:
+            raise HTTPException(status_code=400, detail='invalid request')
+    else:
+        raise HTTPException(status_code=400, detail='invalid request')
+
+    if resp is None:
+        return None
+
+    return crypto_service.encrypt(resp)
 
 
 class NoteRepository:
@@ -124,6 +148,28 @@ class CryptoService:
         return self.__cipher_suite.encrypt(json.dumps(data).encode('utf-8'))
 
 
+class NoteService:
+
+    def __init__(self, note_repo: NoteRepository):
+        self.__note_repo = note_repo
+
+    def get_one(self, uid: int) -> dict[str, Any]:
+        return self.__note_repo.get_one(uid)
+
+    def get_all(self) -> list[dict[str, Any]]:
+        return self.__note_repo.get_all()
+
+    def post(self, note: dict[str, Any]) -> int:
+        return self.__note_repo.save(note)
+
+    def put(self, note: dict[str, Any]) -> None:
+        self.__note_repo.save(note)
+
+    def delete(self, uid: int) -> None:
+        self.__note_repo.delete(uid)
+
+
 connect = sqlite3.connect('gnote.db')
 note_repository = NoteRepository(connect)
+note_service = NoteService(note_repository)
 crypto_service = CryptoService('key.txt')
