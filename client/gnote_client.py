c3f4be52f52a53f7d8194acb87b4b87023d868ce
Marat Gabitov <gabitovmt@yandex.ru>
2025-06-27 21:57:04 +0500

    Реализовал ввод ключа, если его нет в application.properties

--- client/gnote_client.py
+++ client/gnote_client.py
@@ -1,3 +1,5 @@
+import sys
+from tkinter import Tk, Entry, StringVar
 import json
 
 from cryptography.fernet import Fernet
@@ -5,10 +7,8 @@ from cryptography.fernet import Fernet
 
 class CryptoService:
 
-    def __init__(self, key_filename: str):
-        with open(key_filename) as f:
-            key = f.read().rstrip()
-            self.__cipher_suite = Fernet(key)
+    def __init__(self, key: str):
+        self.__cipher_suite = Fernet(key)
 
     def decrypt(self, data):
         return json.loads(self.__cipher_suite.decrypt(data).decode('utf-8'))
@@ -24,12 +24,42 @@ def load_properties() -> dict[str, str]:
             if '=' not in line:
                 continue
             key, value = line.rstrip().split('=', maxsplit=1)
-            print('->', key, value)
             d[key] = value
 
     return d
 
+def enter_password() -> str:
+    tk = Tk()
+
+    tk.title('Gnote')
+    tk.geometry('300x32')
+    tk.resizable(False, False)
+    tk.iconbitmap(default='favicon.ico')
+
+    value = StringVar()
+    entry = Entry(tk, textvariable=value)
+    entry.pack(fill='x', padx=8, pady=8)
+    entry.focus()
+    entry.bind('<Return>', lambda e: tk.destroy())
+
+    tk.mainloop()
+
+    return value.get()
+
+
+def main():
+    props = load_properties()
+
+    if 'key' not in props:
+        key = enter_password()
+        if not key:
+            sys.exit(1)
+        props['key'] = key
+
+    crypto_service = CryptoService(props['key'])
+
+    print(props)
+
 
 if __name__ == '__main__':
-    props = load_properties()
-    crypto_service = CryptoService(props['key'])
+    main()
